7113622abe37b576f33d2c32323fad96ceaa9de2
renzokuken <monipute@renzokuken.eu>
2016-11-23 14:38:36 +0100

    + nqp

---
+++ sites/noquarterprod.rb
@@ -0,0 +1,68 @@
+$: << File.dirname(__FILE__)
+
+require "classe.rb"
+
+class NQP < Classe
+    require "net/http"
+    require "nokogiri"
+
+    def get_content()
+        @alread_fetched_urls = []
+        return get_products(@parsed_content)
+    end
+
+    def get_products(noko)
+        res = []
+        real_products = noko.css("li.purchasable")
+        if real_products.empty? 
+            noko.css("li.product a").each do |p|
+                url = p.attr('href')
+                next if @alread_fetched_urls.include?(url)
+                @alread_fetched_urls << url
+                root = Nokogiri::HTML(Net::HTTP.get(URI(url)))
+                res = res.concat(get_products(root))
+            end
+            return res
+        else
+            real_products.each do |p|
+                url = p.css('a').attr('href').text
+                img_url = p.css('a img').attr('src').text
+                title = p.css('a h3').text
+                price = p.css('span.amount').text
+                res << {"url"=> url,
+                        "img_url" => img_url,
+                        "title"=>title,
+                        "price"=>price,
+                }
+            end
+            return res.uniq
+        end
+    end
+
+    def content_to_html()
+        message_html = ""
+        unless @content==""
+            message_html=<<EOM
+<html>
+<body>
+<ul>
+EOM
+            @content.each do |item|
+                message_html +="<li><a href='#{item["url"]}'><img src='#{item["img_url"]}'> #{item["title"]} - #{item["price"]} </a></li>\n"
+            end
+            message_html+= <<EOM
+</ul>
+</body>
+</html>
+EOM
+        end
+        return message_html
+    end
+
+end
+
+
+NQP.new(url:  "http://www.noquarterprod.com/product-category/carpenter-brut-en/",
+              every: 12*60*60, 
+              test: __FILE__ == $0
+             )
