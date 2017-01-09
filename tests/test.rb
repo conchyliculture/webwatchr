3ab147b7645c6b803f2019c7829f765c9cfafbf5
renzokuken <monipute@renzokuken.eu>
2017-01-09 15:27:37 +0100

    split classes

--- tests/test.rb
+++ tests/test.rb
@@ -5,18 +5,17 @@ require "logger"
 require "pp"
 require "test/unit"
 
-$: << File.join(File.dirname(__FILE__),"..")
-$: << File.join(File.dirname(__FILE__),"..","sites-available")
-
 class TestClasse < Test::Unit::TestCase
-    require "classe.rb"
+    require_relative "../lib/site.rb"
     require "webrick"
+
     $wwwroot = File.join(File.dirname(__FILE__), 'www-root')
     $wwwport = 8001
     $content_is_string = "content_is_string.html"
     $content_is_array = "content_is_array.html"
     $CONF = JSON.parse(File.read(File.join(File.dirname(__FILE__),"..","config.json.template")))
     $CONF["last_dir"] = Dir.mktmpdir
+
     class TestFileHandler < WEBrick::HTTPServlet::FileHandler
         def do_GET(req, res)
             res.status = 200
@@ -55,12 +54,20 @@ class TestClasse < Test::Unit::TestCase
         FileUtils.remove_entry_secure($CONF["last_dir"])
     end
 
+    class TestStringSite < Site::String
+        def get_content()
+            @parsed_content.css("div.content").text
+        end
+    end
+
     def testContentIsString
+
+        content_html = "👌 👌 👌 💯 💯 💯 💯 -KYop-R11Iqo.mp"
+        whole_html = """<!DOCTYPE html>
+<meta charset=\"utf-8\">
+<title>test</title><div class='content'>#{content_html}</div>"""
         File.open(File.join($wwwroot, $content_is_string), "w") do |f|
-            f.puts """<!DOCTYPE html>
-            <meta charset=\"utf-8\">
-            <title>test</title>
-            👌 👌 👌 💯 💯 💯 💯 -KYop-R11Iqo.mp"""
+            f.write whole_html
         end
         url = "http://localhost:#{$wwwport}/#{$content_is_string}"
         wait = 10*60
@@ -69,31 +76,31 @@ class TestClasse < Test::Unit::TestCase
 
         $CONF["alert_proc"] = Proc.new{|x| result = x.to_s.encode('utf-8')}
 
-        c = Classe.new(url: url)
+        c = TestStringSite.new(url: url)
         empty_last = {"content"=>nil, "time"=>-9999999999999}
         assert_equal(empty_last,c.read_last())
         assert_equal(true, c.should_check?(empty_last["time"]))
         assert_equal(false, c.should_check?((Time.now() - wait + 30).to_i))
         html = c.fetch_url(url)
-        assert_equal(File.read(File.join($wwwroot,$content_is_string)), html)
+        assert_equal(whole_html, html)
         assert_equal("test", c.parse_noko(html).css('title').text)
         assert_block{c.last_file().end_with?("last-2182cd5c8685baed48f692ed72d7a89f")}
         c.update()
-        first_pass_content = "{:content=>#{File.read(File.join($wwwroot,$content_is_string)).inspect}"
-        assert_equal("#{first_pass_content}, :name=>\"#{url}\"}", result)
+        first_pass_content = "<!DOCTYPE html>\n<meta charset=\"utf-8\">\n#{content_html}"
+        assert_equal("{:content=>#{first_pass_content.inspect}, :name=>\"#{url}\"}", result)
 
-        File.open(File.join($wwwroot,$content_is_string),"a+") do |f|
-            f.puts "new!"
+        File.open(File.join($wwwroot,$content_is_string),"w+") do |f|
+            f.write whole_html.gsub("</div>"," new ! </div>")
         end
-        c = Classe.new(url: url)
+        c = TestStringSite.new(url: url)
         c.update()
-        assert_equal("#{first_pass_content}, :name=>\"#{url}\"}", result)
+        assert_equal("{:content=>#{first_pass_content.inspect}, :name=>\"#{url}\"}", result)
         c.wait = 0
         c.update()
-        assert_equal("{:content=>#{File.read(File.join($wwwroot,$content_is_string)).inspect}, :name=>\"http://localhost:#{$wwwport}/#{$content_is_string}\"}", result)
+        assert_equal("{:content=>#{(first_pass_content+" new ! ").inspect}, :name=>\"#{url}\"}", result)
     end
 
-    class TestArraySite < Classe
+    class TestArraySite < Site::Articles
         def get_content()
             res=[]
             @parsed_content.css("div").each do |x|
@@ -105,8 +112,12 @@ class TestClasse < Test::Unit::TestCase
     end
 
     def testContentIsArray
+        whole_html = """<!DOCTYPE html>
+<meta charset=\"utf-8\">
+<title>test</title>👌 👌 👌 💯 💯 💯 💯 -KYop-R11Iqo.mp"""
+        whole_html += "<div> lol - lilo</div> <div> fi - fu</div>"
         File.open(File.join($wwwroot, $content_is_array), "w") do |f|
-            f.puts """<!DOCTYPE html> <meta charset=\"utf-8\"> <title>test</title> <U+1F44C> <U+1F44C> <U+1F44C> <U+1F4AF> <U+1F4AF> <U+1F4AF> <U+1F4AF> -KYop-R11Iqo.mp <div> lol - lilo</div> <div> fi - fu</div>"""
+            f.write whole_html
         end
         url = "http://localhost:#{$wwwport}/#{$content_is_array}"
         wait = 10*60
@@ -121,22 +132,22 @@ class TestClasse < Test::Unit::TestCase
         assert_equal(true, c.should_check?(empty_last["time"]))
         assert_equal(false, c.should_check?((Time.now() - wait + 30).to_i))
         html = c.fetch_url(url)
-        assert_equal("<!DOCTYPE html> <meta charset=\"utf-8\"> <title>test</title> <U+1F44C> <U+1F44C> <U+1F44C> <U+1F4AF> <U+1F4AF> <U+1F4AF> <U+1F4AF> -KYop-R11Iqo.mp <div> lol - lilo</div> <div> fi - fu</div>\n", html)
+        assert_equal(whole_html, html)
         assert_equal("test", c.parse_noko(html).css('title').text)
         assert_block{c.last_file().end_with?("last-35e711989b197f20f3d4936e91a2c079")}
         c.update()
-        expected_result ="""<!DOCTYPE html>\n<meta charset=\"utf-8\">\n<ul style=\"list-style-type: none;\">\n<li><a href=' lol '> lilo </a></li>\n<li><a href=' fi '> fu </a></li>\n\n</ul>"""
-		assert_equal("{:content=>#{expected_result.inspect}, :name=>\"#{url}\"}", result)
+        expected_html = "<!DOCTYPE html>\n<meta charset=\"utf-8\">\n<ul style=\"list-style-type: none;\">\n<li><a href=' lol '> lilo </a></li>\n<li><a href=' fi '> fu </a></li>\n\n</ul>"
+		assert_equal("{:content=>#{expected_html.inspect}, :name=>\"#{url}\"}", result)
 
         File.open(File.join($wwwroot,$content_is_array),"a+") do |f|
-            f.puts "<div>new! - new </div>"
+            f.write "<div>new! - new </div>"
         end
         c = TestArraySite.new(url: url)
         c.update()
-		assert_equal("{:content=>#{expected_result.inspect}, :name=>\"#{url}\"}", result)
+		assert_equal("{:content=>#{expected_html.inspect}, :name=>\"#{url}\"}", result)
         c.wait = 0
         c.update()
-        expected_result ="""<!DOCTYPE html>\n<meta charset=\"utf-8\">\n<ul style=\"list-style-type: none;\">\n<li><a href='new! '> new  </a></li>\n\n</ul>"""
-        assert_equal("{:content=>#{expected_result.inspect}, :name=>\"http://localhost:#{$wwwport}/#{$content_is_array}\"}", result)
+        expected_html.gsub!("\n</ul>", "<li><a href='new! '> new  </a></li>\n\n</ul>")
+        assert_equal("{:content=>#{expected_html.inspect}, :name=>\"http://localhost:#{$wwwport}/#{$content_is_array}\"}", result)
     end
 end
