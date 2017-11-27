eb5144e3fbd6f9f08f6c396e7c6a2b8c7cb81e16
renzokuken <github@renzokuken.eu>
2017-11-27 12:52:09 +0100

    user power assert

--- tests/test.rb
+++ tests/test.rb
@@ -9,7 +9,7 @@ class TestClasse < Test::Unit::TestCase
     require_relative "../lib/site.rb"
     require "webrick"
 
-    $wwwroot = File.join(File.dirname(__FILE__), 'www-root')
+    $wwwroot = File.join(File.dirname(__FILE__), "www-root")
     $wwwport = 8001
     $content_is_string = "content_is_string.html"
     $content_is_array = "content_is_array.html"
@@ -76,23 +76,23 @@ class TestClasse < Test::Unit::TestCase
 
         result = ""
 
-        $CONF["alert_proc"] = Proc.new{|x| result = x.to_s.encode('utf-8')}
+        $CONF["alert_proc"] = Proc.new{|x| result = x.to_s.encode("utf-8")}
 
         c = TestStringSite.new(url: url, comment:"comment")
-        assert_equal({}, c.load_state_file())
+        assert {c.load_state_file() == {}}
         empty_last = {"content"=>nil, "time"=>-9999999999999}
-        assert_equal(true, c.should_update?(empty_last["time"]))
-        assert_equal(false, c.should_update?((Time.now() - wait + 30).to_i))
+        assert {c.should_update?(empty_last["time"]) == true}
+        assert {c.should_update?((Time.now() - wait + 30).to_i) == false}
         html = c.fetch_url(url)
-        assert_equal(whole_html, html)
-        assert_equal("test", c.parse_noko(html).css('title').text)
-        assert_block{c.state_file().end_with?("last-2182cd5c8685baed48f692ed72d7a89f")}
+        assert {whole_html == html}
+        assert {c.parse_noko(html).css("title").text == "test"}
+        assert {c.state_file().end_with?("last-2182cd5c8685baed48f692ed72d7a89f")}
         c.update()
         expected_error = "DEBUG -- : Alerting new stuff"
         last_error = $logger_test_io.string.split("\n")[-1]
-        assert(last_error.end_with?(expected_error), "last_error should end with: #{expected_error}\n, but is #{last_error}")
+        assert {last_error.end_with?(expected_error)}
         first_pass_content = Site::HTML_HEADER + content_html
-        assert_equal("{:content=>#{first_pass_content.inspect}, :name=>\"#{url} (comment)\"}", result)
+        assert {"{:content=>#{first_pass_content.inspect}, :name=>\"#{url} (comment)\"}" == result}
 
         File.open(File.join($wwwroot,$content_is_string),"w+") do |f|
             f.write whole_html.gsub("</div>"," new ! </div>")
@@ -101,26 +101,26 @@ class TestClasse < Test::Unit::TestCase
         c.update()
         expected_error = "INFO -- : Too soon to update #{url}"
         last_error = $logger_test_io.string.split("\n")[-1]
-        assert(last_error.end_with?(expected_error), "last_error should end with: #{expected_error}\n, but is #{last_error}")
-        assert_equal("{:content=>#{first_pass_content.inspect}, :name=>\"#{url} (comment)\"}", result)
+        assert {last_error.end_with?(expected_error)}
+        assert {"{:content=>#{first_pass_content.inspect}, :name=>\"#{url} (comment)\"}" == result}
+
         c.wait = 0
         c.update()
         expected_error = "DEBUG -- : Alerting new stuff"
         last_error = $logger_test_io.string.split("\n")[-1]
-        assert(last_error.end_with?(expected_error), "last_error should end with: #{expected_error}\n, but is #{last_error}")
-        assert_equal("{:content=>#{(first_pass_content+" new ! ").inspect}, :name=>\"#{url} (lol)\"}", result)
-        expected_last = {"url"=>url, "wait"=>0, "content"=>content_html+" new ! "}
+        assert {last_error.end_with?(expected_error)}
+        assert {"{:content=>#{(first_pass_content+" new ! ").inspect}, :name=>\"#{url} (lol)\"}" == result}
         result_last = JSON.parse(File.read(c.state_file))
         result_last.delete("time")
-        assert_equal(expected_last, result_last)
+        assert {result_last == {"url"=>url, "wait"=>0, "content"=>content_html+" new ! "}}
     end
 
     class TestArraySite < Site::Articles
         def get_content()
             res=[]
             @parsed_content.css("div").each do |x|
-                a,b = x.text.split('-').map{|s| s.strip()}
-                add_article({'id' => a, "url" => a, "title"=>b})
+                a,b = x.text.split("-").map{|s| s.strip()}
+                add_article({"id" => a, "url" => a, "title"=>b})
             end
             return res
         end
@@ -138,30 +138,30 @@ class TestClasse < Test::Unit::TestCase
 
         result = ""
         called = false
-        $CONF["alert_proc"] = Proc.new{|x| result = x.to_s.encode('utf-8'); called = true}
+        $CONF["alert_proc"] = Proc.new{|x| result = x.to_s.encode("utf-8"); called = true}
 
         c = TestArraySite.new(url: url, every: wait)
         empty_last = {"content"=>nil, "time"=>-9999999999999}
-        assert_equal({}, c.load_state_file())
-        assert_equal(true, c.should_update?(empty_last["time"]))
-        assert_equal(false, c.should_update?((Time.now() - wait + 30).to_i))
+        assert {c.load_state_file() == {}}
+        assert {c.should_update?(empty_last["time"])}
+        assert { ! c.should_update?((Time.now() - wait + 30).to_i)}
         html = c.fetch_url(url)
-        assert_equal(whole_html, html)
-        assert_equal("test", c.parse_noko(html).css('title').text)
-        assert_block{c.state_file.end_with?("last-35e711989b197f20f3d4936e91a2c079")}
+        assert {html == whole_html}
+        assert {c.parse_noko(html).css("title").text == "test"}
+        assert {c.state_file.end_with?("last-35e711989b197f20f3d4936e91a2c079")}
 
         # First full run, Get 2 things
         c.update()
         expected_error = "DEBUG -- : Alerting new stuff"
         last_error = $logger_test_io.string.split("\n")[-1].strip()
-        assert(last_error.end_with?(expected_error), "last_error should end with: '#{expected_error}', but is '#{last_error}'")
+        assert {last_error.end_with?(expected_error)}
         expected_html = Site::HTML_HEADER.dup + [
             "<ul style='list-style-type: none;'>",
             "<li id='lol'><a href='lol'>lilo</a></li>",
             "<li id='fi'><a href='fi'>fu</a></li>",
             "</ul>"].join("\n")
-		assert_equal("{:content=>#{expected_html.inspect}, :name=>\"#{url}\"}", result)
-        assert_equal(true, called)
+		assert {"{:content=>#{expected_html.inspect}, :name=>\"#{url}\"}" == result}
+        assert {called}
 
         result = ""
         called = false
@@ -174,9 +174,9 @@ class TestClasse < Test::Unit::TestCase
         c.update()
         expected_error = "INFO -- : Too soon to update #{url}"
         last_error = $logger_test_io.string.split("\n")[-1]
-        assert(last_error.end_with?(expected_error), "last_error should end with: #{expected_error}\n, but is #{last_error}")
-        assert_equal(false, called)
-		assert_equal("", result)
+        assert {last_error.end_with?(expected_error)}
+        assert { ! called}
+		assert {result == ""}
 
         result = ""
         called = false
@@ -186,13 +186,13 @@ class TestClasse < Test::Unit::TestCase
         c.update()
         expected_error = "DEBUG -- : Alerting new stuff"
         last_error = $logger_test_io.string.split("\n")[-1]
-        assert(last_error.end_with?(expected_error), "last_error should end with: #{expected_error}\n, but is #{last_error}")
+        assert {last_error.end_with?(expected_error)}
         expected_html = Site::HTML_HEADER.dup + [
             "<ul style='list-style-type: none;'>",
             "<li id='new!'><a href='new!'>new</a></li>",
             "</ul>"].join("\n")
-        assert_equal(true, called)
-        assert_equal("{:content=>#{expected_html.inspect}, :name=>\"#{url}\"}", result)
+        assert {called}
+        assert {result == "{:content=>#{expected_html.inspect}, :name=>\"#{url}\"}"}
         expected_last = {"url"=>"http://localhost:8001/content_is_array.html",
                          "wait"=>0,
                          "content"=>[{"id"=>"lol", "title"=>"lilo", "url"=>"lol"},
@@ -203,7 +203,7 @@ class TestClasse < Test::Unit::TestCase
         result_last["content"].each do |item|
             item.delete("_timestamp")
         end
-        assert_equal(expected_last, result_last)
+        assert {expected_last == result_last}
 
         result = ""
         called = false
@@ -213,12 +213,12 @@ class TestClasse < Test::Unit::TestCase
         c.update()
         expected_error = "INFO -- : Nothing new for #{url}"
         last_error = $logger_test_io.string.split("\n")[-1]
-        assert(last_error.end_with?(expected_error), "last_error should end with: #{expected_error}\n, but is #{last_error}")
+        assert {last_error.end_with?(expected_error)}
         expected_html = Site::HTML_HEADER.dup + [
             "<ul style=\"list-style-type: none;\">",
             "</ul>"].join("\n")
-        assert_equal(false, called)
-		assert_equal("", result)
+        assert {!called}
+		assert {result == ""}
 
         result = ""
         called = false
@@ -230,11 +230,11 @@ class TestClasse < Test::Unit::TestCase
         c.update()
         expected_error = "ERROR -- : Network error on #{url} : Failed to open TCP connection to localhost:8001 (Connection refused - connect(2) for \"localhost\" port 8001). Will retry in 0 + 30 minutes"
         last_error = $logger_test_io.string.split("\n")[-1]
-        assert(last_error.end_with?(expected_error), "last_error should end with: #{expected_error}\n, but is #{last_error}")
+        assert {last_error.end_with?(expected_error)}
         expected_html = Site::HTML_HEADER.dup + [
             "<ul style=\"list-style-type: none;\">",
             "</ul>"].join("\n")
-        assert_equal(false, called)
-		assert_equal("", result)
+        assert {! called}
+		assert {result == ""}
     end
 end
