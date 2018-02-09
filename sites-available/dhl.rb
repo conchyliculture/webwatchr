ceaf9cf02a287e0eaa087f623adc2a82fc4ab124
renzokuken <github@renzokuken.eu>
2018-02-09 14:16:12 +0100

    fix dhl

--- sites-available/dhl.rb
+++ sites-available/dhl.rb
@@ -4,33 +4,18 @@
 require_relative "../lib/site.rb"
 
 class DHL < Site::SimpleString
-    require "date"
-    require "json"
-
-    # Here we want to do check only part of the DOM.
-    #   @html_content contains the HTML page as String
-    #   @parsed_content contains the result of Nokogiri.parse(@html_content)
-    #
     def get_content()
-        res = ""
-        j = JSON.parse(@html_content)
-        unless j['results']
-            raise Site::ParseError("Please verify the DHL tracking ID #{@url}")
+        div = @parsed_content.css('div.well-status')
+        if div.empty?
+            raise Site::ParseError.new "Please verify the DHL tracking ID"
         end
-        j['results'][0]["checkpoints"].each do |l|
-            descr = l['description']
-            time = DateTime.strptime("#{l['date']} #{l['time']}","%A, %B %d, %Y %H:%M")
-            location = l['location']
-            res << "#{time} : #{descr} (#{location})<br/>\n"
-        end
-        return res
+        return div.text.gsub("\t", "")
     end
-
 end
 
-trackingnb="0000000000"
+trackingnb = "000000000000"
 DHL.new(
-    url:  "http://www.dhl.com/shipmentTracking?AWB=#{trackingnb}",
+    url:  "https://nolp.dhl.de/nextt-online-public/en/search?piececode=#{trackingnb}",
     every: 60*60,
    test: __FILE__ == $0
 ).update
