921a5670f95b30de58d7d720dc74a25b6d154862
renzokuken <github@renzokuken.eu>
2018-03-02 11:39:24 +0100

    +songkick

---
+++ sites-available/songkick.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/ruby
+# encoding: utf-8
+
+require_relative "../lib/site.rb"
+
+class Songkick < Site::Articles
+
+    def get_content()
+        @parsed_content.css('ul.event-listings li').each do |event|
+            next if event["class"] =~ /with-date/
+
+            date = event.css("time")[0]["datetime"].gsub("T", " ")
+            url = "https://www.songkick.com" + event.css('p a')[0]["href"]
+            artist = event.css("p a span").text.strip
+            location = event.css('p.location').text.gsub(/\s+/, " ")
+            add_article({
+                "id"=> url,
+                "url"=> url,
+                "title" => "#{date}: #{artist} at #{location}"
+            })
+        end
+    end
+end
+    
+# example
+#Songkick.new(
+#    #   url: https://www.songkick.com/artists/7214659-carpenter-brut/calendar
+#    every: 12 * 60 * 60,
+#    comment: "CarpenterBrut concerts",
+#    test: __FILE__ == $0
+#).update
