856ec71d62c1713830a6b5d61a4889b624582f9b
frenchsplainer <6418006+conchyliculture@users.noreply.github.com>
2019-07-11 00:02:01 +0200

    ricardo fixed their cert

--- sites-available/ricardo.rb
+++ sites-available/ricardo.rb
@@ -3,22 +3,6 @@
 require_relative "../lib/site.rb"
 require "openssl"
 
-# Ricard is broken and uses terrible SSL signature methods
-# This means this script can't work with OpenSSL 1.1
-# The workaround would be to patch the SSL Context for the
-# http object to 1
-# http://ruby-doc.org/stdlib-2.6/libdoc/openssl/rdoc/OpenSSL/SSL/SSLContext.html#method-i-security_level-3D
-begin
-    level = OpenSSL::SSL::SSLContext.new().security_level
-    if level
-        raise Exception.new("Sorry, your openssl library is too recent and won't work with Ricardo's crappy certificates")
-    end
-rescue NoMethodError
-    # We fail, so we are using an old openssl versio, that will 
-    # work with Ricardo
-end
-
-
 class Ricardo < Site::Articles 
     def initialize(search_term:, every:, comment:nil, test:false)
         super(
