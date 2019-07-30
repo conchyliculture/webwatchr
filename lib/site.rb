8dddbd2818b337a48a4fae12a82b6f0948b51d09
frenchsplainer <6418006+conchyliculture@users.noreply.github.com>
2019-07-30 13:29:23 +0200

    disable fake Chrome UA

--- lib/site.rb
+++ lib/site.rb
@@ -54,7 +54,7 @@ class Site
             else
                 req = Net::HTTP::Get.new(uri)
             end
-            req["User-Agent"] = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.75 Safari/537.36"
+#            req["User-Agent"] = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.75 Safari/537.36"
             response = http.request(req)
             case response.code
             when "301", "302"
