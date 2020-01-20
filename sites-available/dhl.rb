72314e381dfaa71fa02a7be6350abb6989bf82ab
frenchsplainer <6418006+conchyliculture@users.noreply.github.com>
2020-01-20 15:53:51 +0100

    fix dhl

--- sites-available/dhl.rb
+++ sites-available/dhl.rb
@@ -3,29 +3,39 @@
 
 require_relative "../lib/site.rb"
 
+require "json"
+
 class DHL < Site::SimpleString
 
     def initialize(track_id:, every:, comment:nil, test:false)
         super(
-            url:  "https://nolp.dhl.de/nextt-online-public/en/search?piececode=#{track_id}",
+            url:  "https://www.dhl.com/shipmentTracking?AWB=#{track_id}",
             every: every,
             test: test,
             comment: comment,
         )
     end
     def get_content()
-        div = @parsed_content.css('div.well-status')
-        if div.empty?
-            raise Site::ParseError.new "Please verify the DHL tracking ID"
-        end
-        return div.text.gsub("\t", "").gsub("international shipment", "shipment").gsub(/Next step.*$/m, "").gsub("export parcel center", "destination parcel center")
+        res = ""
+#        begin
+            j = JSON.parse(@html_content)
+            status = j.dig("results",0, "delivery", "status")
+            res << status + "\n"
+            j.dig("results", 0, "checkpoints"). each do |update|
+              res << "#{update['date']} #{update['time']}: #{update['description']}\n"
+            end
+#        rescue
+#            raise Site::ParseError.new "Please verify the DHL tracking ID"
+#        end
+
+        return res
+
     end
 end
 
 # example:
 # DHL.new(
-#     track_id: "000000000000",
+#     track_id: "0000000000",
 #     every: 60*60,
 #     test: __FILE__ == $0
 # ).update
-# 
