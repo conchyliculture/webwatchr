06eb9fc6331c123b58cd0441b935e136521dacfb
frenchsplainer <6418006+conchyliculture@users.noreply.github.com>
2020-01-28 00:19:12 +0100

    weird ebay URLs

--- sites-available/ebay.rb
+++ sites-available/ebay.rb
@@ -15,11 +15,14 @@ class Ebay < Site::Articles
     def get_content()
         @parsed_content.css('li.s-item').each do |article|
             a = article.css('div.s-item__image a')
+            url = nil
             if a.size > 0
-              url = a[0]['href']
-            else
-              next
+              uri = a[0]['href']
+              url = URI.parse(uri)
+              url = "https://"+url.host+url.path
             end
+
+            next unless url
 
             image = article.css('img.s-item__image-img')[0]['src']
             title = article.css('h3.s-item__title')[0].text
