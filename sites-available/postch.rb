551371858e42b03f7096e1f22e65e6e2aa554ebc
Renzo <6418006+conchyliculture@users.noreply.github.com>
2020-08-29 12:49:16 +0200

    Fix post.ch website (#7)

--- sites-available/postch.rb
+++ sites-available/postch.rb
@@ -3,26 +3,93 @@
 
 require_relative "../lib/site.rb"
 
+begin
+  require "curb"
+rescue LoadError => e
+  puts "HTTP/2 requires the curb gem"
+  raise e
+end
+require "json"
+
+
 class PostCH < Site::SimpleString
 
-    def initialize(track_id:, every:, comment:nil, test:false)
+    attr_accessor :messages
+    def initialize(track_id:, every:, messages:nil, comment:nil, test:false)
         super(
             url: "https://service.post.ch/EasyTrack/submitParcelData.do?formattedParcelCodes=#{track_id}",
             every: every,
             test: test,
             comment: comment,
         )
+        @track_id = track_id
+        @messages = messages
+
+        if not @messages
+          build_messages()
+        end
     end
+
+    def build_messages()
+        c = Curl.get("https://service.post.ch/ekp-web/core/rest/translations/en/shipment-text-messages.json")
+        c.perform
+        messages = JSON.parse(c.body_str)
+        shipment_text_old = messages["shipment-text--"]
+        @messages = shipment_text_old.dup
+        shipment_text_old.each do |k, v|
+          if k=~/PARCEL\.\*\.\*\.([0-9]+)/
+            @messages["PARCEL.*.#{$1}"] = v
+          end
+        end
+    end
+
+    def pull_things()
+        # First we need an anonymous userId
+        c = Curl.get("https://service.post.ch/ekp-web/api/user")
+        c.set(:HTTP_VERSION, Curl::HTTP_2_0)
+        c.perform
+        user_id = JSON.parse(c.body_str)["userIdentifier"][13..-1]
+        _, *http_headers = c.header_str.split(/[\r\n]+/).map(&:strip)
+        http_headers = Hash[http_headers.flat_map{ |s| s.scan(/^(\S+): (.+)/) }]
+        cookie = http_headers["set-cookie"][/^([^;]+);/,1]
+
+        # Then a hash for the shipment
+        c = Curl.post("https://service.post.ch/ekp-web/api/history?userId=%3C%5Banonymous%5D%3E#{user_id}", {"searchQuery" => @track_id}.to_json)
+        c.set(:HTTP_VERSION, Curl::HTTP_2_0)
+        c.headers["content-type"] = "application/json"
+        c.headers["x-csrf-token"] = http_headers["x-csrf-token"]
+        c.headers["Cookie"] = cookie
+        c.perform
+        hash = JSON.parse(c.body_str)["hash"]
+
+        # Then a corresponding identity
+        c = Curl.get("https://service.post.ch/ekp-web/api/history/not-included/#{hash}?userId=%3C%5Banonymous%5D%3E#{user_id}")
+        c.set(:HTTP_VERSION, Curl::HTTP_2_0)
+        c.perform
+        @global_state = JSON.parse(c.body_str)[0]
+        identity = @global_state["identity"]
+
+        # And now the list of events
+        c = Curl.get("https://service.post.ch/ekp-web/api/shipment/id/#{identity}/events/")
+        c.set(:HTTP_VERSION, Curl::HTTP_2_0)
+        c.perform
+        @events = JSON.parse(c.body_str)
+    end
+
     def get_content()
         res = []
-        table = @parsed_content.css("table.events_view tr").map{|row| row.css("td").map{|r| r.text.strip}}.delete_if{|x| x.empty?}
-        if table.size==0
-            raise Site::ParseError.new "Please verify the PostCH tracking ID"
+        if @global_state["deliveryRange"]
+          res << "Expected delivery time: between #{@global_state["deliveryRange"]["start"]} and #{@global_state["deliveryRange"]["end"]}"
         end
-        table.each do |r|
-            res << "#{r[0]} - #{r[1]} : #{r[3]}: #{r[2].split("\n")[-1].strip()}<br/>\n"
+        res << "<ul>"
+        @events.sort{|x, y| y["timestamp"] <=> x["timestamp"]}.each do |event|
+          if event["eventCode"]=~/PARCEL\.\*\..\.([0-9]+)$/
+            event["eventCode"] = "PARCEL\.\*\.#{$1}"
+          end
+          res << "<li>[#{event['timestamp']}] #{@messages[event["eventCode"]]} (#{(event["city"].to_s+ " "+event["country"].to_s).strip()})</li>"
         end
-        return res.join("")
+        res << "</ul>"
+        return res.join("\n")
     end
 end
 
