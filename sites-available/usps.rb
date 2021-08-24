5cf411df6726c7cfcc5239f85f75cb53ee717448
frenchsplainer <6418006+conchyliculture@users.noreply.github.com>
2021-08-24 23:24:58 +0200

    replace usps with WebAPI

--- sites-available/usps.rb
+++ sites-available/usps.rb
@@ -1,11 +1,13 @@
 #!/usr/bin/ruby
 # encoding: utf-8
 
+require "nokogiri"
 require_relative "../lib/site.rb"
 
 class USPS < Site::SimpleString
 
     def initialize(track_id:, every:, comment:nil, test:false)
+        puts "This is most likely broken :("
         super(
             url: "https://tools.usps.com/go/TrackConfirmAction?tLabels=#{track_id}",
             every: every,
@@ -19,6 +21,7 @@ class USPS < Site::SimpleString
     def clean(node)
       return node.text.delete("\r\n").gsub("\t", " ").gsub(/  +/, " ").strip()
     end
+
 
     def get_content()
         infos = @parsed_content.css('div.thPanalAction')
@@ -36,8 +39,52 @@ class USPS < Site::SimpleString
 
 end
 
+class USPSAPI < Site::SimpleString
+    def initialize(track_id:, username: nil, every:, comment:nil, test:false)
+      unless username
+        raise Exception.new('USPS requires you to register to their Web API at https://registration.shippingapis.com/ for fetching tracking information.')
+      end
+      @username = username
+      @track_id = track_id
+      builder = Nokogiri::XML::Builder.new do |xml|
+        xml.TrackRequest('USERID' => @username) {
+          xml.TrackID('ID' => @track_id)
+        }
+      end
+
+      super(
+        url: "https://secure.shippingapis.com/ShippingAPI.dll?API=TrackV2&XML=#{builder.to_xml}",
+        every: every,
+        test: test,
+        comment: comment
+      )
+    end
+
+    def get_content()
+      pp Nokogiri::XML.parse(@html_content)
+      root = Nokogiri::XML.parse(@html_content)
+      res = [root.css("TrackResponse TrackSummary")]
+      res << "<ul>"
+      root.css("TrackResponse TrackInfo TrackDetail").each do |deet|
+        res << "<li>"+deet.text+"</li>"
+      end
+      res << "</ul>"
+      return res.join("\n")
+    end
+
+end
+
 # Example:
 #
+#USPSAPI.new(
+#    username: "111OOOOO2222",
+#    track_id: "UE000000000US",
+#    every: 60*60,
+#    test: __FILE__ == $0
+#).update()
+#
+#
+# This is most likely broken, use USPSAPI class above instead
 # USPS.new(
 #     track_id: "LZ000000000US",
 #     every: 60*60,
