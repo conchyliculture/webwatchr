b5f77933e4b6dfb4721bd067640bb431c826f5b0
frenchsplainer <6418006+conchyliculture@users.noreply.github.com>
2022-02-23 22:51:38 +0100

    add checklist to trello

--- sites-available/trello.rb
+++ sites-available/trello.rb
@@ -6,7 +6,8 @@ require "json"
 require "mechanize"
 
 class TrelloStruct
-  def initialize(json)
+  def initialize(mechanize, json)
+    @mechanize = mechanize
     @name = json['name']
     @lists = {}
     @cards = []
@@ -17,24 +18,51 @@ class TrelloStruct
       add_card(card['name'], card['id'], card['idList'])
     end
   end
+
   def add_list(name, id)
     @lists[id] = name
   end
+
   def add_card(name, id, list_id)
-    @cards << {name: name, id:id, list_id:list_id}
+    @cards << {name: name, id:id, list_id:list_id, details: get_card_details(name, id)}
   end
+
+  def get_card_details(name, id)
+    url = "https://trello.com/1/cards/#{id}?checklists=all"
+    res = @mechanize.get(url)
+    card_json = JSON.parse(res.body)
+    return card_json
+  end
+
   def get_cards_by_list_id(list_id)
     return @cards.select{|c| c[:list_id] == list_id}
   end
+
   def to_html(ignores:[])
     res = ""
     @lists.each do |li,ln|
       next if ignores.include?(ln)
       res << "<b>#{ln}</b> \n<ul>\n"
       get_cards_by_list_id(li).each do |c|
-        res << "  <li>#{c[:name]}</li>\n"
+        card_html = "  <li>"
+        card_html << "#{c[:name]}\n"
+        if c[:details]
+          if c[:details]['checklists']
+            c[:details]['checklists'].each do |checklist|
+              card_html << "    <ul>"
+              checklist['checkItems'].each do |checkitem|
+                card_html << "      <li>"
+                card_html << "#{checkitem['name']}: #{checkitem['state']}"
+                card_html << "<li>\n"
+              end
+              card_html << "<\/ul>\n"
+            end
+          end
+        end
+        card_html += "<\/li>\n"
+        res << card_html
       end
-      res << "</ul>\n"
+      res << "<\/ul>\n"
     end
     return res
   end
@@ -50,19 +78,20 @@ class Trello < Site::DiffString
         )
         @json = nil
         @ignores = ignores
+        @mechanize = nil
     end
 
     def pull_things()
-      mechanize = Mechanize.new
-      mechanize.get(@url)
+      @mechanize = Mechanize.new
+      @mechanize.get(@url)
       trello_id = @url[/b\/([^\/]+)/,1]
-      mechanize.get("https://trello.com/1/board/#{trello_id}?fields=id,idOrganization")
-      res = mechanize.get("https://trello.com/1/Boards/#{trello_id}?lists=open&list_fields=name%2Cclosed%2CidBoard%2Cpos%2Csubscribed%2Climits%2Ccr eationMethod%2CsoftLimit&cards=visible&card_attachments=cover&card_stickers=true&card_fields=badges%2CcardRole%2Cclosed%2Cd ateLastActivity%2Cdesc%2CdescData%2Cdue%2CdueComplete%2CdueReminder%2CidAttachmentCover%2CidList%2CidBoard%2CidMembers%2Cid Short%2CidLabels%2Climits%2Cname%2Cpos%2CshortUrl%2CshortLink%2Csubscribed%2Curl%2ClocationName%2Caddress%2Ccoordinates%2Cc over%2CisTemplate%2Cstart%2Clabels&card_checklists=none&enterprise=true&enterprise_fields=displayName&members=all&member_fi elds=activityBlocked%2CavatarUrl%2Cbio%2CbioData%2Cconfirmed%2CfullName%2CidEnterprise%2CidMemberReferrer%2Cinitials%2Cmemb erType%2CnonPublic%2Cproducts%2Curl%2Cusername&membersInvited=all&membersInvited_fields=activityBlocked%2CavatarUrl%2Cbio%2 CbioData%2Cconfirmed%2CfullName%2CidEnterprise%2CidMemberReferrer%2Cinitials%2CmemberType%2CnonPublic%2Cproducts%2Curl%2Cus ername&memberships_orgMemberType=true&checklists=none&organization=true&organization_fields=name%2CdisplayName%2Cdesc%2Cdes cData%2Curl%2Cwebsite%2Cprefs%2Cmemberships%2ClogoHash%2Cproducts%2Climits%2CidEnterprise%2CpremiumFeatures&organization_ta gs=true&organization_enterprise=true&organization_disable_mock=true&myPrefs=true&fields=name%2Cclosed%2CdateLastActivity%2C dateLastView%2CdatePluginDisable%2CenterpriseOwned%2CidOrganization%2Cprefs%2CpremiumFeatures%2CshortLink%2CshortUrl%2Curl% 2CcreationMethod%2CidEnterprise%2Cdesc%2CdescData%2CidTags%2Cinvitations%2Cinvited%2ClabelNames%2Climits%2Cmemberships%2Cpo werUps%2Csubscribed%2CtemplateGallery&pluginData=true&organization_pluginData=true&boardPlugins=true")
+      @mechanize.get("https://trello.com/1/board/#{trello_id}?fields=id,idOrganization")
+      res = @mechanize.get("https://trello.com/1/Boards/#{trello_id}?lists=open&list_fields=name%2Cclosed%2CidBoard%2Cpos%2Csubscribed%2Climits%2Ccr eationMethod%2CsoftLimit&cards=visible&card_attachments=cover&card_stickers=true&card_fields=badges%2CcardRole%2Cclosed%2Cd ateLastActivity%2Cdesc%2CdescData%2Cdue%2CdueComplete%2CdueReminder%2CidAttachmentCover%2CidList%2CidBoard%2CidMembers%2Cid Short%2CidLabels%2Climits%2Cname%2Cpos%2CshortUrl%2CshortLink%2Csubscribed%2Curl%2ClocationName%2Caddress%2Ccoordinates%2Cc over%2CisTemplate%2Cstart%2Clabels&card_checklists=none&enterprise=true&enterprise_fields=displayName&members=all&member_fi elds=activityBlocked%2CavatarUrl%2Cbio%2CbioData%2Cconfirmed%2CfullName%2CidEnterprise%2CidMemberReferrer%2Cinitials%2Cmemb erType%2CnonPublic%2Cproducts%2Curl%2Cusername&membersInvited=all&membersInvited_fields=activityBlocked%2CavatarUrl%2Cbio%2 CbioData%2Cconfirmed%2CfullName%2CidEnterprise%2CidMemberReferrer%2Cinitials%2CmemberType%2CnonPublic%2Cproducts%2Curl%2Cus ername&memberships_orgMemberType=true&checklists=none&organization=true&organization_fields=name%2CdisplayName%2Cdesc%2Cdes cData%2Curl%2Cwebsite%2Cprefs%2Cmemberships%2ClogoHash%2Cproducts%2Climits%2CidEnterprise%2CpremiumFeatures&organization_ta gs=true&organization_enterprise=true&organization_disable_mock=true&myPrefs=true&fields=name%2Cclosed%2CdateLastActivity%2C dateLastView%2CdatePluginDisable%2CenterpriseOwned%2CidOrganization%2Cprefs%2CpremiumFeatures%2CshortLink%2CshortUrl%2Curl% 2CcreationMethod%2CidEnterprise%2Cdesc%2CdescData%2CidTags%2Cinvitations%2Cinvited%2ClabelNames%2Climits%2Cmemberships%2Cpo werUps%2Csubscribed%2CtemplateGallery&pluginData=true&organization_pluginData=true&boardPlugins=true")
       @json = JSON.parse(res.body)
     end
 
     def get_content()
-      t=TrelloStruct.new(@json)
+      t=TrelloStruct.new(@mechanize, @json)
       return t.to_html(ignores: @ignores)
     end
 end
