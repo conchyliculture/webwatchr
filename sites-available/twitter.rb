2f607657e6534d03e0e07bb1d92f4f3947331e5e
frenchsplainer <6418006+conchyliculture@users.noreply.github.com>
2023-09-05 14:53:47 +0200

    change instance

--- sites-available/twitter.rb
+++ sites-available/twitter.rb
@@ -2,14 +2,18 @@ require_relative "../lib/site.rb"
 
 class Twitter < Site::Articles
 
-    def initialize(account:, regex:nil, with_replies:true, no_retweets:false, every: 60*60 ,test: false, nitter_instance: "nitter.42l.fr")
+    def initialize(account:, regex:nil, with_replies:true, no_retweets:false, every: 60*60 ,test: false, nitter_instance: "nitter.unixfox.eu")
         @regex = regex
         @no_retweets = no_retweets
         @account = account
         if regex.class == String
           @regex = /#{regex.class}/i
         end
-        super(url: "https://#{nitter_instance}/#{account}#{with_replies ? '/with_replies' : ''}", every: every, test: test)
+        super(url: "https://#{nitter_instance}/#{account}#{with_replies ? '/with_replies' : ''}", every: every, test: test, useragent: "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/117.0")
+
+        @extra_headers = {
+          'Accept-Language'=> "en-US,en;q=0.5",
+        }
         @state_file = ".lasts/last-twitter_#{account.downcase}"
     end
 
