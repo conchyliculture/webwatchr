9d3f5fe837c39390a44a962e50cde06f39bc919a
frenchsplainer <6418006+conchyliculture@users.noreply.github.com>
2024-04-17 13:06:58 +0200

    + postse

---
+++ sites-available/postse.rb
@@ -0,0 +1,60 @@
+#!/usr/bin/ruby
+
+require_relative "../lib/site.rb"
+
+require "json"
+require "logger"
+require "net/http"
+
+class PostSE < Site::SimpleString
+    def initialize(track_id:, every:, comment:nil, test:false)
+        super(
+            url:  "https://api2.postnord.com/rest/shipment/v1/trackingweb/shipmentInformation?shipmentId=#{track_id}&locale=en",
+            every: every,
+            test: test,
+            comment: comment,
+        )
+        @events = []
+    end
+
+    def get_html_content()
+      res << Site::HTML_HEADER
+      res << "<ul>"
+      @events.each do|event|
+        res << "<li>#{@event}</li>"
+      end
+      res += @events
+      res << "</ul>"
+      return res.join("\n")
+    end
+
+    def pull_things
+        headers = {'accept' => 'application/json, text/plain, */*',
+                    "x-bap-key" => 'web-tracking-sc'}
+        uri = URI.parse(@url)
+        res = Net::HTTP.get(uri, headers)
+        @parsed_content = JSON.parse(res)
+    end
+
+    def get_content
+        @parsed_content['items'][0]["events"].each do |event|
+          loc = event['location']
+          location = loc['countryCode']
+          if loc['name']
+            location = "#{loc['name']}, #{location}"
+          end
+          @events << "#{event['eventTime']}: #{event['eventDescription']} (#{location})"
+        end
+        @content = @events.join("\n") 
+        return @content
+    end
+
+end
+
+# Example:
+#
+# PostSE.new(
+#     track_id: "LY000000000SE",
+#     every: 30*60,
+#     test: __FILE__ == $0
+# ).update
