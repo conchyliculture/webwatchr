7c05dd382fb3e7b4fb78d5930e2b2448044fd975
frenchsplainer <6418006+conchyliculture@users.noreply.github.com>
2024-12-15 23:01:30 +0100

    fix gls

--- sites-available/gls.rb
+++ sites-available/gls.rb
@@ -1,37 +1,28 @@
-#!/usr/bin/ruby
-# encoding: utf-8
-
-require_relative "../lib/site.rb"
-
-begin
-  require "curb"
-rescue LoadError => e
-  puts "GLS.rb requires the curb gem, because of their shitty SSL version"
-  raise e
-end
+require_relative "../lib/site"
 require "json"
 
-
 class GLS < Site::SimpleString
+  attr_accessor :messages
 
-    attr_accessor :messages
-    def initialize(track_id:, postal_code:, every:, comment:nil, test:false)
-        super(
-          url: "https://gls-group.eu/app/service/open/rest/FR/en/rstt027?match=#{track_id}&postalCode=#{postal_code}&type=&caller=witt002&millis=#{Time.now.to_i}#{rand(1000)}",
-            every: every,
-            test: test,
-            comment: comment,
-        )
-        @track_id = track_id
-    end
+  def initialize(track_id:, postal_code:, every:, comment: nil, test: false)
+    super(
+      url: "https://gls-group.eu/app/service/open/rest/FR/en/rstt027?match=#{track_id}&postalCode=#{postal_code}&type=&caller=witt002&millis=#{Time.now.to_i}#{rand(1000)}",
+      every: every,
+      test: test,
+      comment: comment,
+   )
+    @track_id = track_id
+  end
 
-    def get_content()
-      res = "<ul><li>"
+  def get_content
+    res = "<ul><li>"
 
-      res << JSON.parse(@html_content)["tuStatus"][0]["history"].map{|x| "#{x['date']} #{x['time']}: #{x['evtDscr']} (#{x['address'].values.sort.map(&:strip).join(' ')})"}.join("</li><li>")
+    res << JSON.parse(@html_content)["tuStatus"][0]["history"].map do |x|
+      "#{x['date']} #{x['time']}: #{x['evtDscr']} (#{x['address'].values.sort.map(&:strip).join(' ')})"
+    end.join("</li><li>")
 
-      res << "</li></ul>"
-    end
+    res << "</li></ul>"
+  end
 end
 
 # Example:
