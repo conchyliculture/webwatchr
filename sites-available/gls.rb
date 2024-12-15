8b60cff63272b2163e72e7177605fe8446141d60
frenchsplainer <6418006+conchyliculture@users.noreply.github.com>
2024-12-15 23:42:36 +0100

    fix gls again

--- sites-available/gls.rb
+++ sites-available/gls.rb
@@ -5,13 +5,13 @@ class GLS < Site::SimpleString
   attr_accessor :messages
 
   def initialize(track_id:, postal_code:, every:, comment: nil, test: false)
+    @state_file_name = "last-gls-#{track_id}-#{postal_code}"
     super(
       url: "https://gls-group.eu/app/service/open/rest/FR/en/rstt027?match=#{track_id}&postalCode=#{postal_code}&type=&caller=witt002&millis=#{Time.now.to_i}#{rand(1000)}",
       every: every,
       test: test,
       comment: comment,
    )
-    @state_file_name = "last-gls-#{track_id}-#{postal_code}"
     @track_id = track_id
   end
 
