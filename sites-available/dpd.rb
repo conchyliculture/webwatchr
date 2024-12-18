f3c0751326a910945ee802875b97883f4b3894c4
frenchsplainer <6418006+conchyliculture@users.noreply.github.com>
2024-12-18 21:17:53 +0100

    fix dpd

--- sites-available/dpd.rb
+++ sites-available/dpd.rb
@@ -1,38 +1,40 @@
 #!/usr/bin/ruby
-# encoding: utf-8
 
-require_relative "../lib/site.rb"
+require_relative "../lib/site"
 
 require "json"
-require "pp"
 
 class DPD < Site::SimpleString
+  def initialize(track_id:, every:, comment: nil, test: false)
+    super(
+      url: "https://tracking.dpd.de/rest/plc/en_US/#{track_id}",
+      every: every,
+      test: test,
+      comment: comment,
+    )
+    @track_id = track_id
+  end
 
-    def initialize(track_id:, every:, comment:nil, test:false)
-        super(
-          url: "https://tracking.dpd.de/rest/plc/en_US/#{track_id}",
-          every: every,
-          test: test,
-          comment: comment,
-        )
-    end
+  def get_content
+    res = ["<ul>"]
+    j = JSON.parse(@html_content)
+    if j
+      unless j["parcellifecycleResponse"]["parcelLifeCycleData"]
+        return "No info for #{@track_id}"
+      end
 
-    def get_content()
-        res = ["<ul>"]
-        j = JSON.parse(@html_content)
-        if j
-          j["parcellifecycleResponse"]["parcelLifeCycleData"]["statusInfo"].select{|i| i["statusHasBeenReached"]}.each{|x|
-            date = x["date"]
-            place = x["location"]
-            descr = x["description"]["content"].join(' ')
-            res << "<li>#{date} #{place} #{descr}</li>"
-          }
-        else
-            raise Site::ParseError.new "Please verify the DPD tracking ID"
-        end
-        res << ["</ul>"]
-        return res.join("\n")
+      j["parcellifecycleResponse"]["parcelLifeCycleData"]["statusInfo"].select { |i| i["statusHasBeenReached"] }.each { |x|
+        date = x["date"]
+        place = x["location"]
+        descr = x["description"]["content"].join(' ')
+        res << "<li>#{date} #{place} #{descr}</li>"
+      }
+    else
+      raise Site::ParseError, "Please verify the DPD tracking ID"
     end
+    res << ["</ul>"]
+    return res.join("\n")
+  end
 end
 
 # Example:
