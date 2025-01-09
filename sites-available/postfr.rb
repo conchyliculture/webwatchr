0aa68bd936fe3528d41c17fefa98e54303e91a23
frenchsplainer <6418006+conchyliculture@users.noreply.github.com>
2025-01-09 14:21:52 +0100

    lint

--- sites-available/postfr.rb
+++ sites-available/postfr.rb
@@ -3,23 +3,23 @@ require_relative "../lib/site"
 require "json"
 
 class PostFR < Site::SimpleString
+  def initialize(track_id:, every:, comment: nil, test: false)
+    super(
+      url: "https://www.laposte.fr/ssu/sun/back/suivi-unifie/#{track_id}?lang=en_GB",
+      every: every,
+      test: test,
+      comment: comment,
+    )
+  end
 
-    def initialize(track_id:, every:, comment:nil, test:false)
-        super(
-            url: "https://www.laposte.fr/ssu/sun/back/suivi-unifie/#{track_id}?lang=en_GB",
-            every: every,
-            test: test,
-            comment: comment,
-        )
+  def get_content
+    res = []
+    j = JSON.parse(@html_content)
+    j[0]['shipment']['event'].each do |e|
+      res << "#{e['date']}: #{e['label']} (country: #{e['country']})"
     end
-    def get_content()
-        res = []
-        j = JSON.parse(@html_content)
-        j[0]['shipment']['event'].each do |e|
-          res << "#{e['date']}: #{e['label']} (country: #{e['country']})" 
-        end
-        return res.join("\n")
-    end
+    return res.join("\n")
+  end
 end
 
 # Example:
