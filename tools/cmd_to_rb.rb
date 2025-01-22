a057b34089729ef8de42b1cdfcd7542d65b8b2cc
frenchsplainer <6418006+conchyliculture@users.noreply.github.com>
2025-01-22 16:17:56 +0100

    add header

--- tools/cmd_to_rb.rb
+++ tools/cmd_to_rb.rb
@@ -1,3 +1,17 @@
+# Pass a file where you copied a "Saved as curl" command from your browser
+#
+# This will generate a ruby script, multilined, easy to comment out lines, that will generate the same command, for testing
+#
+# Example:
+# ruby _cmd_to_rb /tmp/curl.sh > /tmp/curl.rb
+#
+# hack hack on /tmp/curl.rb to remove cookies, headers, etc.
+#
+# ruby /tmp/curl.rb > /tmp/newcurl.sh
+# bash /tmp/newcurl.sh
+#
+# and make sure you get the results you need
+#
 data = File.read(ARGV[0])
 
 unless data.start_with?("curl")
