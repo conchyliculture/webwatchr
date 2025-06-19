8692938e19c54a3cce8ad63ae0e77c9f4db0b3d5
frenchsplainer <6418006+conchyliculture@users.noreply.github.com>
2025-06-19 23:47:19 +0200

    add postnl

---
+++ lib/sites/postnl.rb
@@ -0,0 +1,54 @@
+require_relative "../webwatchr/site"
+
+# example:
+#
+#  update PostNL do
+#    track_id "XX102917683NL"
+#  end
+
+class PostNL < Site::SimpleString
+  require "net/http"
+  require "json"
+
+  def track_id(track_id)
+    # Sets the Track ID & URL
+    @track_id = track_id
+    @url = "https://www.postnl.post/track?barcodes=#{track_id}"
+    self
+  end
+
+  def initialize
+    super()
+    @parsed_json = nil
+  end
+
+  def pull_things()
+    resp = Net::HTTP.post(URI.parse("https://postnl.post/api/v1/auth/token"), nil, nil)
+    token = JSON.parse(resp.body)["access_token"]
+
+    resp = Net::HTTP.post(
+      URI.parse("https://postnl.post/api/v1/tracking-items"), {
+        "items" => ["CH188699083NL"],
+        "language_code" => "en"
+      }.to_json,
+      {
+        'Content-Type' => 'application/json',
+        'Authorization' => "Bearer #{token}"
+      }
+    )
+    @parsed_json = JSON.parse(resp.body)
+  end
+
+  def extract_content()
+    res = []
+    @parsed_json['data']['items'][0]['events'].each do |event|
+      msg = "#{event['datetime_local']}: #{event['status_description']}"
+      if event['country_code']
+        msg << " (#{event['country_code']})"
+      end
+      res << msg
+    end
+
+    return ResultObject.new(res.join(""))
+  end
+end
