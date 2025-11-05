10fbc36bf211f0f690d58534d8760e51613c353f
frenchsplainer <6418006+conchyliculture@users.noreply.github.com>
2025-11-05 20:02:11 +0100

    add quickpac

---
+++ lib/sites/quickpac.rb
@@ -0,0 +1,31 @@
+require_relative "../webwatchr/site"
+
+require "json"
+
+class Quickpac < Site::SimpleString
+  def track_id(track_id)
+    case track_id
+    when /^[-1-9]{18}$/
+      @track_id = track_id.scan(/^(..)(..)(......)(........)$/)[-1].join(".")
+    when /^[-1-9]{2}\.[0-9]{2}\.[0-9]{6}\.[0-9]{8}$/
+      @track_id = track_id
+    else
+      raise Site::ParseError, "track_id should either in fortmat 121212345612345680 or 12.12.123456.12345678"
+    end
+    @url = "https://parcelsearch.quickpac.ch/api/ParcelSearch/GetPublicTracking/#{track_id}/en/false"
+  end
+
+  def get_content()
+    @parsed_content = JSON.parse(@website_html)
+    res = Site::SimpleString::ListResult.new()
+    if not @parsed_content or not @parsed_content["Protocol"]
+      raise Site::ParseError, "Nothing found for #{@track_id}. Check it is correct."
+    end
+
+    @parsed_content["Protocol"].each do |p|
+      res << "#{p['Time']}: #{p['StatusText']}"
+    end
+
+    return res
+  end
+end
